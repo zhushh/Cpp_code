b6f4dd7381b90e4eca65aa9c3c1fa8e148a55d3f
朱术煌 <838044048@qq.com>
2017-03-10 16:13:53 +0800

    Update Socket.cpp

--- Socket/src/Socket.cpp
+++ Socket/src/Socket.cpp
@@ -1,3 +1,5 @@
+#include "stdafx.h"
+
 #include "Socket.h"
 
 // class Socket {
@@ -6,13 +8,14 @@
 //    virtual bool recv(string) = 0;
 // };
 
-Server::Server(int port) { init("127.0.0.1", port); }
-Server::Server(char* ip, int port) { init(ip, port); }
-Server::~Server() { closesocket(clientSock);  }
+Server::Server(int port) { initBindingAndListening("127.0.0.1", port); }
+Server::Server(char* ip, int port) { initBindingAndListening(ip, port); }
+Server::~Server() { closesocket(clientSock); }
 
-void Server::init(char*ip, int port) {
+void Server::initBindingAndListening(char*ip, int port) {
+	cout << "start init" << endl;
 	servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	IF_COND_PRINT_AND_EXIT(servSock < 0, "Create socket fail!\n", -1);
+	IF_COND_PRINT_AND_EXIT(servSock == INVALID_SOCKET, "Create socket fail!\n", -1);
 
 	// init sockaddr_in 
 	struct sockaddr_in servAddr;
@@ -23,24 +26,25 @@ void Server::init(char*ip, int port) {
 
 	// binding
 	int retVal = bind(servSock, (sockaddr*)&servAddr, sizeof(servAddr));
-	IF_COND_PRINT_AND_EXIT(retVal < 0,"Bind socket fail!\n", -1);
+	IF_COND_PRINT_AND_EXIT(retVal == SOCKET_ERROR, "Bind socket fail!\n", -1);
+	printf("Binding succeed!\n");
 
 	// listening
-	IF_COND_PRINT_AND_EXIT(listen(servSock, 10) < 0, "Listening fail!\n", -1);
+	IF_COND_PRINT_AND_EXIT(listen(servSock, 10) == SOCKET_ERROR, "Listening fail!\n", -1);
 	printf("Server %d is listening...\n", port);
 }
 
-void Server::start() {
+bool Server::isAccepted() {
 	memset(&clientAddr, 0, sizeof(sockaddr_in));
 	int addrlen = sizeof(clientAddr);
 	clientSock = accept(servSock, (sockaddr*)&clientAddr, &addrlen);
 	IF_COND_PRINT_AND_EXIT(clientSock < 0, "Accept client fail!\n", -1);
 	printf("Accept from %s\n", inet_ntoa(clientAddr.sin_addr));
+	return true;
 }
 
-bool Server::send(string str) {}
-bool Server::recv(string str) {}
-
+bool Server::send(string str) { return true; }
+bool Server::recv(string str) { return true; }
 
 Client::Client() {}
 Client::Client(char* ip, int port) { connectTo(ip, port); }
@@ -48,17 +52,18 @@ Client::~Client() { closesocket(servSock); }
 
 bool Client::connectTo(char* ip, int port) {
 	servSock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	IF_COND_PRINT_AND_EXIT(servSock < 0, "Create socket fail!\n", -1);
+	IF_COND_PRINT_AND_EXIT(servSock == INVALID_SOCKET, "Create socket fail!\n", -1);
 	struct sockaddr_in servAddr;
 	servAddr.sin_family = AF_INET;
 	servAddr.sin_port = htons(port);
 	servAddr.sin_addr.S_un.S_addr = inet_addr(ip);
 
 	int retVal = connect(servSock, (sockaddr*)&servAddr, sizeof(servAddr));
-	IF_COND_PRINT_AND_EXIT(retVal < 0, "Connect to server fail!\n", -1);
+	IF_COND_PRINT_AND_EXIT(retVal == SOCKET_ERROR, "Connect to server fail!\n", -1);
 	printf("Connect to server %s:%d succeed!\n", ip, port);
+	return true;
 }
 
-bool Client::send(string str) {}
-bool Client ::recv(string str) {}
+bool Client::send(string str) { return true; }
+bool Client::recv(string str) { return true; }
 
